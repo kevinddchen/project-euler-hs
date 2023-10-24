8554fe07b7bb3490ee2f7f92e98d2a01c66e7f79
Kevin Chen <79341521+kevinddchen@users.noreply.github.com>
2023-10-23 23:29:45 -0700

    add p6

---
+++ src/p6.hs
@@ -0,0 +1,8 @@
+square :: (Num a) => a -> a
+square x = x * x
+
+result :: Int
+result = let nums = [1..100] in (square . sum $ nums) - (sum . map square $ nums)
+
+main :: IO ()
+main = print result
