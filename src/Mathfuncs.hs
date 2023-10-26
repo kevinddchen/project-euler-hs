d27fec1cb7b6eb1e9b9326242eb1b76a12d4515a
Kevin Chen <79341521+kevinddchen@users.noreply.github.com>
2023-10-25 23:54:48 -0700

    update docstrings

--- src/Mathfuncs.hs
+++ src/Mathfuncs.hs
@@ -4,12 +4,15 @@ module Mathfuncs
   )
 where
 
+-- | Helper function for `isPrime`, which recursively checks whether n is
+-- divisible by an odd number.
 _isPrime :: (Integral a) => a -> a -> Bool
-_isPrime p n
-  | p * p > n = True
-  | n `mod` p == 0 = False
-  | otherwise = _isPrime (p + 2) n
+_isPrime k n
+  | k * k > n = True
+  | n `mod` k == 0 = False
+  | otherwise = _isPrime (k + 2) n
 
+-- | Returns True if the given number is prime.
 isPrime :: (Integral a) => a -> Bool
 isPrime n
   | n <= 1 = False
@@ -24,6 +27,8 @@ _divOut p n
   | n `mod` p /= 0 = (0, n)
   | otherwise = let (k, m) = _divOut p (n `div` p) in (k + 1, m)
 
+-- | Helper function for `primeFactorize`, which recursively finds the number
+-- of times p divides n for all p.
 _primeFactorize :: (Integral a) => a -> a -> [(a, a)]
 _primeFactorize p n
   | pTooBig && n /= 1 = [(n, 1)]
@@ -34,7 +39,7 @@ _primeFactorize p n
     pTooBig = p * p > n
     (k, nextN) = _divOut p n
     nextFacts = _primeFactorize nextP nextN
-    nextP = if p `mod` 2 == 1 then p + 2 else p + 1
+    nextP = if p == 2 then p + 1 else p + 2
 
 -- | Returns the prime factorization of an integer as a list of (prime,
 -- exponent) pairs. For example, primeFactorize 12 == [(2,2),(3,1)].
